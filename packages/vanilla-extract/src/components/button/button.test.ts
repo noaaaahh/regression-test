b7dcac1c6454ca1fa3955da3ff3301ad0613fe11
noah.choi <noah.choi@goorm.io>
2025-03-02 16:09:00 +0900

    test

--- packages/vanilla-extract/src/components/button/button.test.ts
+++ packages/vanilla-extract/src/components/button/button.test.ts
@@ -10,6 +10,32 @@ test("button", async ({ page }, workerInfo) => {
   await page.waitForSelector("#storybook-root");
   await page.waitForLoadState("domcontentloaded");
 
+  // DOM 안정 상태 감지를 위한 스크립트를 페이지에 주입
+  await page.addInitScript(() => {
+    // no mutation이 발생하면 50ms 후 자동 resolve
+    window.waitForDomStable = new Promise<void>((resolve) => {
+      const observer = new MutationObserver(() => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+          observer.disconnect();
+          resolve();
+        }, 50);
+      });
+      let timer = setTimeout(() => {
+        observer.disconnect();
+        resolve();
+      }, 50);
+      observer.observe(document, {
+        attributes: true,
+        childList: true,
+        subtree: true,
+      });
+    });
+  });
+
+  // DOM이 안정된 상태가 될 때까지 대기
+  await page.evaluate(() => window.waitForDomStable);
+
   await expect(page).toHaveScreenshot(
     `vanilla-button--test-bed-${workerInfo.project.name}.png`,
     { fullPage: true, animations: "disabled" }
