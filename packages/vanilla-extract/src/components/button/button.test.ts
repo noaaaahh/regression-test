29251fc23178273c5288c9cd02a3b906b4116a03
noah.choi <noah.choi@goorm.io>
2025-03-02 22:27:18 +0900

    test

--- packages/vanilla-extract/src/components/button/button.test.ts
+++ packages/vanilla-extract/src/components/button/button.test.ts
@@ -46,21 +46,7 @@ test("button", async ({ page }) => {
 
   await page.goto(`/iframe.html?${params.toString()}`);
   await page.waitForSelector("#storybook-root");
-
-  await page.waitForTimeout(5000);
   await page.waitForLoadState("domcontentloaded");
-  await page.waitForLoadState("load");
-  await page.waitForLoadState("networkidle");
-
-  await page.waitForTimeout(5000);
-  await page.waitForLoadState("domcontentloaded");
-  await page.waitForLoadState("load");
-  await page.waitForLoadState("networkidle");
-
-  await page.waitForTimeout(5000);
-  await page.waitForLoadState("domcontentloaded");
-  await page.waitForLoadState("load");
-  await page.waitForLoadState("networkidle");
 
   // 안정된 DOM 상태에서 스냅샷을 찍습니다.
   await expect(page).toHaveScreenshot({
@@ -71,7 +57,7 @@ test("button", async ({ page }) => {
 
 // import { test, expect } from "@playwright/test";
 
-// test("button", async ({ page }, workerInfo) => {
+// test("button", async ({ page }) => {
 //   const params = new URLSearchParams({
 //     id: "vanilla-button--test-bed",
 //     viewMode: "story",
@@ -80,32 +66,6 @@ test("button", async ({ page }) => {
 //   await page.goto(`/iframe.html?${params.toString()}`);
 //   await page.waitForSelector("#storybook-root");
 //   await page.waitForLoadState("domcontentloaded");
-
-//   await page.addInitScript(() => {
-//     // no mutation이 발생하면 50ms 후 자동 resolve
-//     window.waitForDomStable = new Promise<void>((resolve) => {
-//       const observer = new MutationObserver(() => {
-//         clearTimeout(timer);
-//         timer = setTimeout(() => {
-//           observer.disconnect();
-//           resolve();
-//         }, 50);
-//       });
-//       let timer = setTimeout(() => {
-//         observer.disconnect();
-//         resolve();
-//       }, 50);
-//       observer.observe(document, {
-//         attributes: true,
-//         childList: true,
-//         subtree: true,
-//       });
-//     });
-//   });
-
-//   // DOM이 안정된 상태가 될 때까지 대기
-//   await page.evaluate(() => window.waitForDomStable);
-//   await page.waitForTimeout(10000);
 
 //   await expect(page).toHaveScreenshot({
 //     fullPage: true,
