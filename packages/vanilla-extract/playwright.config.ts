92a4d62f295eb3dbe31f1f47de72563e0dae669d
noah.choi <noah.choi@goorm.io>
2025-03-03 22:24:05 +0900

    chore: trace on every visual regression test

--- packages/vanilla-extract/playwright.config.ts
+++ packages/vanilla-extract/playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
 
   // ...
   reporter: [["html", { outputFolder: "./tests/report" }]],
-  use: { baseURL: BASE_URL, trace: "retain-on-failure" },
+  use: { baseURL: BASE_URL, trace: "on" },
   projects: [
     {
       name: "chromium",
