cb25879ea3fa12bdb2e6b188b2740802c01e8646
noah.choi <noah.choi@goorm.io>
2025-03-03 13:21:26 +0900

    test

--- packages/vanilla-extract/playwright.config.ts
+++ packages/vanilla-extract/playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"], channel: "chromium" },
+      use: { ...devices["Desktop Chrome"], channel: "chrome" },
     },
   ],
 
