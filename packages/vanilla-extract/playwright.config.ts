616f11253e39499c6bb8bfc7fb007f7e8e5844f2
noah.choi <noah.choi@goorm.io>
2025-04-19 15:00:03 +0900

    x

--- packages/vanilla-extract/playwright.config.ts
+++ packages/vanilla-extract/playwright.config.ts
@@ -1,20 +1,17 @@
-// playwright.config.ts
 import { defineConfig, devices } from "@playwright/test";
 
 const BASE_URL = process.env.TEST_BASE_URL || "http://localhost:6006";
 
 export default defineConfig({
-  snapshotPathTemplate:
-    "./tests/__screenshot__/{arg}-{projectName}-{platform}-{ext}",
-  // snapshotDir: "./tests/__screenshot__",
+  snapshotPathTemplate: "./tests/__screenshot__/{arg}-{projectName}-{platform}-{ext}",
   outputDir: `./tests/results/`,
-  // testDir: "./src/components/**/*.test.*",
 
-  // ...
-  reporter: [["html", { outputFolder: "./tests/report" }]],
+  reporter: [
+    ["html", { outputFolder: "./tests/report" }],
+    ["json", { outputFile: "./tests/report/index.json" }],
+  ],
   use: { baseURL: BASE_URL, trace: "on" },
   projects: [
-    { name: "chromium", use: { ...devices["Desktop Chrome"] } },
     { name: "firefox", use: { ...devices["Desktop Firefox"] } },
     { name: "webkit", use: { ...devices["Desktop Safari"] } },
     {
